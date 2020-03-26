1afc07665262ab3e1d6cf76a406aaa53ee5a1c5e
Marina Riera <marina.rierav@e-campus.uab.cat>
2020-03-26 17:04:18 +0100

    fix cfg

--- configs/exp40_cfg.py
+++ configs/exp40_cfg.py
@@ -166,7 +166,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(384, 288), keep_ratio=True),
-    #dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomFlip', flip_ratio=0.),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
